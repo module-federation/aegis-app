542869f260916f1ac6afbf961d3e6653ce4370ca
tyson <tyson7757@comcast.net>
2021-06-10 10:02:16 -0500

    fix

--- webpack.cache.config.js
+++ webpack.cache.config.js
@@ -9,9 +9,9 @@ var serverConfig = {
   entry: ["@babel/polyfill", path.resolve(__dirname, "src/index.js")],
   output: {
     path: path.resolve(__dirname, "dist"),
-    //publicPath: "http://localhost:8060",
-    publicPath:
-      "https://api.github.com/module-federation/MicroLib-Example/dist?ref=order-server",
+    publicPath: "http://cache.aegis.dev:8060",
+    //publicPath:
+    // "https://api.github.com/module-federation/MicroLib-Example/dist?ref=order-server",
 
     //  publicPath:
     //      "https://api.github.com/module-federation/MicroLib-Example/dist?ref=master",
