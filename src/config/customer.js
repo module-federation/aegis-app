50ad6250e56253c9c03e6894f4edeb359ac38c75
tyson <tyson7757@comcast.net>
2021-06-12 17:06:09 -0500

    CACHE

--- src/config/customer.js
+++ src/config/customer.js
@@ -20,12 +20,12 @@ export const Customer = {
   factory: makeCustomerFactory,
   validate: validateModel,
   onDelete: okToDelete,
-  datasource: {
-    factory: DataSourceAdapterMongoDb,
-    url: "mongodb://localhost:27017",
-    cacheSize: 2000,
-    baseClass: "DataSourceMongoDb",
-  },
+  // datasource: {
+  //   factory: DataSourceAdapterMongoDb,
+  //   url: "mongodb://localhost:27017",
+  //   cacheSize: 2000,
+  //   baseClass: "DataSourceMongoDb",
+  // },
   mixins: [
     freezeProperties("customerId"),
     requireProperties(
