151cef242415b0f12f801dbda093dd1ee032801f
tyson <tyson7757@comcast.net>
2021-07-08 00:54:21 -0500

    jfg

--- src/services/webswitch.js
+++ src/services/webswitch.js
@@ -25,58 +25,6 @@ async function getHostName() {
   return "localhost";
 }
 
-function getHeaders(method, payload) {
-  const contentLength = ["POST", "PATCH"].includes(method)
-    ? Buffer.byteLength(payload)
-    : 0;
-
-  const contentHeaders = { "Content-Type": "application/json" };
-
-  return contentLength > 0
-    ? { ...contentHeaders, "Content-Length": contentLength }
-    : contentHeaders;
-}
-
-async function httpsClient({
-  hostname,
-  port,
-  path,
-  protocol = "https",
-  method = "GET",
-  payload = "",
-  safe = true,
-}) {
-  return new Promise(function (resolve, reject) {
-    const normal = {
-      hostname,
-      port,
-      path,
-      method,
-      headers: getHeaders(method, payload),
-    };
-
-    const options = safe ? normal : { ...normal, rejectUnauthorized: false };
-    const chunks = [];
-    const client = {
-      http: http,
-      https: https,
-    };
-
-    try {
-      const req = client[protocol].request(options, res => {
-        res.setEncoding("utf8");
-        res.on("data", chunk => chunks.push(chunk));
-        res.on("error", e => console.warn(httpsClient.name, e.message));
-        res.on("end", () => resolve(chunks.join("")));
-      });
-      req.on("error", e => reject(e));
-      if (payload) req.on("connect", () => req.write(payload));
-    } catch (e) {
-      console.warn(httpsClient.name, e.message);
-    }
-  });
-}
-
 /**@type import("ws/lib/websocket") */
 let webswitchClient;
 
@@ -87,56 +35,99 @@ export async function publishEvent(event, observer, useWebswitch = true) {
   const serializedEvent = JSON.stringify(event);
 
   try {
-    if (useWebswitch) {
-      console.debug("using webswitch");
+    function webswitch() {
+      console.debug("sending", event);
 
-      function webswitch() {
-        console.debug("calling", event);
+      if (!webswitchClient) {
+        webswitchClient = new WebSocket(`ws://${hostname}:${PORT}${PATH}`);
 
-        if (!webswitchClient) {
-          webswitchClient = new WebSocket(`ws://${hostname}:${PORT}${PATH}`);
-        }
+        // setTimeout(() => {
+        //   webswitchClient.ping();
+        // }, 30000);
 
-        setTimeout(() => {
-          webswitchClient.ping();
-        }, 30000);
+        // const timerId = setTimeout(() => {
+        //   webswitchClient.terminate();
+        //   webswitch();
+        // }, 60000);
 
-        const timerId = setTimeout(() => {
-          webswitchClient.terminate();
-          webswitch();
-        }, 60000);
-
-        webswitchClient.on("pong", function () {
-          clearTimeout(timerId);
-          setTimeout(() => webswitchClient.ping(), 30000);
-        });
-
-        webswitchClient.on("open", function () {
-          console.log("readyState", webswitchClient.readyState);
-          console.debug("sending");
-          webswitchClient.send(serializedEvent);
-        });
+        // webswitchClient.on("pong", function () {
+        //   clearTimeout(timerId);
+        //   setTimeout(() => webswitchClient.ping(), 30000);
+        // });
 
         webswitchClient.on("message", function (message) {
           // const event = JSON.parse(message);
-          console.debug(message);
+          console.debug(messaage);
           observer.notify(event.eventName, event);
         });
-
-        webswitchClient.send(serializedEvent);
       }
 
-      webswitch();
-    } else {
-      httpsClient({
-        hostname,
-        port,
-        path,
-        method: "POST",
-        payload: serialziedEvent,
-      });
+      webswitchClient.send(serializedEvent);
     }
+    webswitch();
   } catch (e) {
     console.warn(publishEvent.name, e.message);
   }
 }
+
+// function getHeaders(method, payload) {
+//   const contentLength = ["POST", "PATCH"].includes(method)
+//     ? Buffer.byteLength(payload)
+//     : 0;
+
+//   const contentHeaders = { "Content-Type": "application/json" };
+
+//   return contentLength > 0
+//     ? { ...contentHeaders, "Content-Length": contentLength }
+//     : contentHeaders;
+// }
+
+// async function httpsClient({
+//   hostname,
+//   port,
+//   path,
+//   protocol = "https",
+//   method = "GET",
+//   payload = "",
+//   safe = true,
+// }) {
+//   return new Promise(function (resolve, reject) {
+//     const normal = {
+//       hostname,
+//       port,
+//       path,
+//       method,
+//       headers: getHeaders(method, payload),
+//     };
+
+//     const options = safe ? normal : { ...normal, rejectUnauthorized: false };
+//     const chunks = [];
+//     const client = {
+//       http: http,
+//       https: https,
+//     };
+
+//     try {
+//       const req = client[protocol].request(options, res => {
+//         res.setEncoding("utf8");
+//         res.on("data", chunk => chunks.push(chunk));
+//         res.on("error", e => console.warn(httpsClient.name, e.message));
+//         res.on("end", () => resolve(chunks.join("")));
+//       });
+//       req.on("error", e => reject(e));
+//       if (payload) req.on("connect", () => req.write(payload));
+//     } catch (e) {
+//       console.warn(httpsClient.name, e.message);
+//     }
+//   });
+// }
+
+// else {
+//   httpsClient({
+//     hostname,
+//     port,
+//     path,
+//     method: "POST",
+//     payload: serialziedEvent,
+//   });
+// }
