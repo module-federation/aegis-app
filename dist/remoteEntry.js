da84ce48102217e769ab4a80023df67924a42860
tyson <tyson7757@comcast.net>
2021-07-29 04:52:06 -0500

    change adapter

--- dist/remoteEntry.js
+++ dist/remoteEntry.js
@@ -31821,7 +31821,7 @@ var moduleMap = {
 		return Promise.all([__webpack_require__.e(777), __webpack_require__.e(579)]).then(() => () => (__webpack_require__(/*! ./src/services/webswitch */ "./src/services/webswitch.js")));
 	},
 	"./wasm": () => {
-		return __webpack_require__.e(357).then(() => () => (__webpack_require__(/*! ./src/wasm */ "./src/wasm/index.js")));
+		return __webpack_require__.e(254).then(() => () => (__webpack_require__(/*! ./src/wasm/demo.js */ "./src/wasm/demo.js")));
 	}
 };
 var get = (module) => {
