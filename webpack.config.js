a0ec74b1062e4f19e75d63b10ea994bee403123a
tyson <trmidboe@gmail.com>
2021-08-27 03:31:37 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ var serverConfig = {
   output: {
     path: path.resolve(__dirname, "dist"),
     publicPath:
-      "https://api.github.com?owner=module-federation&repo=microlib-example&filedir=dist&branch=master",
+      "https://api.github.com?owner=module-federation&repo=microlib-example&filedir=dist&branch=customer3",
     libraryTarget: "commonjs",
   },
   devtool: "source-map",
