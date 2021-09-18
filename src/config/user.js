79960d26e1d5b39e86ffff5003c6d1a4d07c96c5
trmidboe <trmibdoe@gmail.com>
2021-09-18 07:23:25 -0500

    fib\

--- src/config/user.js
+++ src/config/user.js
@@ -20,24 +20,5 @@ export const User = {
       type: 'oneToOne',
       modelName: 'customer'
     }
-  },
-  commands: {
-    runFibonacci: {
-      command: model => {
-        function fibonacci (x) {
-          if (x === 0) {
-            return 0
-          }
-
-          if (x === 1) {
-            return 1
-          }
-
-          return fibonacci(x - 1) + fibonacci(x - 2)
-        }
-        return fibonacci(model.fibonacci)
-      },
-      acl: ['read']
-    }
   }
 }
