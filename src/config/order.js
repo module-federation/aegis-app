2d9fe5b40cdadc8e42176d6a73d8f6c8010de6f1
trmidboe <trmibdoe@gmail.com>
2021-09-18 09:06:54 -0500

    do it

--- src/config/order.js
+++ src/config/order.js
@@ -289,6 +289,22 @@ export const Order = {
     cancel: {
       command: cancel,
       acl: ['write', 'cancel']
+    },
+    runFibonacci: {
+      command: function fibonacci(model) {
+        const x = model.fibonacci || 20 
+
+          if (x === 0) {
+            return 0;
+          }
+        
+          if (x === 1) {
+            return 1;
+          }
+        
+          return fibonacci(x - 1) + fibonacci(x - 2);
+        }
+      }
     }
   },
   serializers: [
