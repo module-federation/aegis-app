8e211f4ca06b0c41ac2ba204d8c0b84510a4a370
trmidboe <trmibdoe@gmail.com>
2021-11-08 08:29:11 -0600

    demo

--- src/config/catalog.js
+++ src/config/catalog.js
@@ -4,5 +4,26 @@
 const Catalog = {
   modelName: 'catalog',
   endpoint: 'catalogs',
-  factory: dependencies => (...args) => Object.freeze({ args: args })
+  factory: dependencies => (...args) => Object.freeze({ args: args }),
+  ports: {
+    myPort: {
+      service: 'myService',
+      timeout: 0,
+      circuitBreaker: {
+        default: {
+          callVolume: 100,
+          intervalMs: 6000,
+          errorRate: 25,
+          fallbackFn: data => {
+            console.log('do this', data)
+          }
+        },
+        conntimeout: {}
+      },
+      consumesEvent: 'executeMyPort',
+      producesEvent: 'myPortExecuted',
+      callback: () => null,
+      type: 'outbound'
+    }
+  }
 }
--- src/config/order.js
+++ src/config/order.js
@@ -46,7 +46,7 @@ export const Order = {
   datasource: {
     factory: DataSourceAdapterMongoDb,
     //url: 'mongodb://localhost:27017',
-    url: 'mongodb://aegis.module-federation.org:27017',
+    url: 'mongodb://localhost:27017',
     cacheSize: 4000,
     baseClass: 'DataSourceMongoDb'
   },
