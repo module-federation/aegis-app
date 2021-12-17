a1332fef75d339c78b63d3935a6b9d29636e247f
tyson <trmidboe@gmail.com>
2021-12-17 01:07:23 -0600

    Delete scripts directory

--- scripts/delete-topics.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-kafka/bin/kafka-topics.sh --delete --topic orderChannel --bootstrap-server localhost:9092
-
-kafka/bin/kafka-topics.sh --delete --topic shippingChannel --bootstrap-server localhost:9092
-
-kafka/bin/kafka-topics.sh --delete --topic inventoryChannel --bootstrap-server localhost:9092
-
-kafka/bin/kafka-topics.sh --delete --topic broadcastChannel --bootstrap-server localhost:9092
