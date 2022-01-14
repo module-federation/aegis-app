191192609a82d8fb3cf58e041e5991f68bb3f6d6
trmidboe <trmibdoe@gmail.com>
2022-01-13 19:33:49 -0600

    demo

--- src/domain/order.js
+++ src/domain/order.js
@@ -685,7 +685,7 @@ export function makeOrderFactory (dependencies) {
       [orderTotal]: total,
       [orderStatus]: OrderStatus.PENDING,
       [orderNo]: dependencies.uuid(),
-      desc: 'new order 13',
+      desc: 'new order 14',
       /**
        * Has payment for the order been authorized?
        */
