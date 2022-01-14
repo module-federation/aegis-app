a3ea2358f14a39180eae6a49e72c5860f5a81ed2
trmidboe <trmibdoe@gmail.com>
2022-01-13 19:05:01 -0600

    specified

--- src/domain/order.js
+++ src/domain/order.js
@@ -685,7 +685,7 @@ export function makeOrderFactory (dependencies) {
       [orderTotal]: total,
       [orderStatus]: OrderStatus.PENDING,
       [orderNo]: dependencies.uuid(),
-      desc: 'new order 12`',
+      desc: 'new order 13',
       /**
        * Has payment for the order been authorized?
        */
