883fe44a29661d349290db10c440b94ad7e3b132
tyson <trmidboe@gmail.com>
2022-01-24 14:18:22 -0600

    Update order.js

--- src/domain/order.js
+++ src/domain/order.js
@@ -685,7 +685,7 @@ export function makeOrderFactory (dependencies) {
       [orderTotal]: total,
       [orderStatus]: OrderStatus.PENDING,
       [orderNo]: dependencies.uuid(),
-      desc: 'new order 21',
+      desc: 'new order 22',
       /**
        * Has payment for the order been authorized?
        */
