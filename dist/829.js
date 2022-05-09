f16a69ba9fe411bd0bf3a705de715431069a45c9
tysonrm <tyson.midboe@gmail.com>
2022-05-08 19:58:09 -0500

    test

--- dist/829.js
+++ dist/829.js
@@ -166,12 +166,12 @@ var Inventory = {
   endpoint: 'inventory',
   dependencies: {},
   factory: _domain_inventory__WEBPACK_IMPORTED_MODULE_1__.makeInventoryFactory,
-  // datasource: {
-  //   factory: DataSourceAdapterMongoDb,
-  //   url: 'mongodb://127.0.0.1:27017',
-  //   cacheSize: 4000,
-  //   baseClass: 'DataSourceMongoDb'
-  // },
+  datasource: {
+    factory: _adapters_datasources_datasource_mongodb__WEBPACK_IMPORTED_MODULE_0__.DataSourceAdapterMongoDb,
+    url: 'mongodb://127.0.0.1:27017',
+    cacheSize: 4000,
+    baseClass: 'DataSourceMongoDb'
+  },
   mixins: [(0,_domain_mixins__WEBPACK_IMPORTED_MODULE_2__.requireProperties)('name', 'inStock', 'category', 'price', 'purchaseOrder'), (0,_domain_mixins__WEBPACK_IMPORTED_MODULE_2__.validateProperties)([{
     propKey: 'inStock',
     "typeof": 'number',
@@ -240,12 +240,12 @@ var Order = {
   modelName: 'order',
   endpoint: 'orders',
   factory: _domain_order__WEBPACK_IMPORTED_MODULE_0__.makeOrderFactory,
-  // datasource: {
-  //   factory: DataSourceAdapterMongoDb,
-  //   url: 'mongodb://127.0.0.1:27017',
-  //   cacheSize: 4000,
-  //   baseClass: 'DataSourceMongoDb'
-  // },
+  datasource: {
+    factory: _adapters_datasources_datasource_mongodb__WEBPACK_IMPORTED_MODULE_2__.DataSourceAdapterMongoDb,
+    url: 'mongodb://127.0.0.1:27017',
+    cacheSize: 4000,
+    baseClass: 'DataSourceMongoDb'
+  },
   dependencies: {
     uuid: function uuid() {
       return (0,nanoid__WEBPACK_IMPORTED_MODULE_3__.nanoid)(8);
--- src/config/order.js
+++ src/config/order.js
@@ -43,12 +43,12 @@ export const Order = {
   modelName: 'order',
   endpoint: 'orders',
   factory: makeOrderFactory,
-  // datasource: {
-  //   factory: DataSourceAdapterMongoDb,
-  //   url: 'mongodb://127.0.0.1:27017',
-  //   cacheSize: 4000,
-  //   baseClass: 'DataSourceMongoDb'
-  // },
+  datasource: {
+    factory: DataSourceAdapterMongoDb,
+    url: 'mongodb://127.0.0.1:27017',
+    cacheSize: 4000,
+    baseClass: 'DataSourceMongoDb'
+  },
   dependencies: { uuid: () => nanoid(8) },
   mixins: [
     requireProperties(
