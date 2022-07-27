373e377ed60f14ca558d9d19660d24078f159f7f
tysonrm <trmidboe@gmail.com>
2022-07-27 11:05:38 -0500

    modified:   dist/remoteEntry.js 	modified:   dist/remoteEntry.js.map

--- dist/remoteEntry.js
+++ dist/remoteEntry.js
@@ -36665,7 +36665,7 @@ module.exports = (batch, sender, Result, keyTranslationFormat) => {
 
 /***/ }),
 
-/***/ "webpack/container/entry/order":
+/***/ "webpack/container/entry/local":
 /*!***********************!*\
   !*** container entry ***!
   \***********************/
@@ -36975,7 +36975,7 @@ module.exports = require("zlib");
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		__webpack_require__.p = "http://localhost:8000/";
+/******/ 		__webpack_require__.p = "https://api.github.com?owner=module-federation&repo=aegis-app&filedir=dist&branch=master";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/sharing */
@@ -37287,7 +37287,7 @@ module.exports = require("zlib");
 /******/ 		// object to store loaded chunks
 /******/ 		// "0" means "already loaded", Promise means loading
 /******/ 		var installedChunks = {
-/******/ 			637: 0
+/******/ 			446: 0
 /******/ 		};
 /******/ 		
 /******/ 		var installChunk = (chunk) => {
@@ -37350,7 +37350,7 @@ module.exports = require("zlib");
 /******/ 	// module exports must be returned from runtime so entry inlining is disabled
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__("webpack/container/entry/order");
+/******/ 	return __webpack_require__("webpack/container/entry/local");
 /******/ })()
 ;
 //# sourceMappingURL=remoteEntry.js.map
