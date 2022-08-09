add0be1b049a42ebc15150503e3b57020c176fde
tysonrm <trmidboe@gmail.com>
2022-08-09 13:11:52 -0500

    fix deleted adapters

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -2,6 +2,12 @@
 
 export * from './service-locator'
 export * from './websocket-adapter'
+export * from './event-adapter'
+export * from './address-adapter'
+export * from './inventory-adapter'
+export * from './order-adapter'
+export * from './payment-adapter'
+export * from './shipping-adapter'
 
 /**
  * @typedef {import('../domain').Model} Model
